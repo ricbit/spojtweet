baf31999c90c8027f34cad1ff4ee23bd9dcb3c3e
Ricardo Bittencourt <bluepenguin@gmail.com>
2011-09-09 22:00:11 -0700

    Adding missing files

---
+++ crawler.py
@@ -0,0 +1,7 @@
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.api import urlfetch
+
+class CrawlCountryPage(webapp.RequestHandler):
+  def get(self):
+    pass    
