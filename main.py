df6872b24722370f8d5c2c86940a810336a8ceda
Ricardo Bittencourt <bluepenguin@gmail.com>
2011-09-14 22:18:32 -0700

    Using django templates 1.2

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import datetime
 import os
 import urllib2
 
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
